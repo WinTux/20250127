7e5ea89d7eabf0f6abdbc6a82607062d30ee64df
WinTux <rusok.software@gmail.com>
2025-01-27 21:21:22 -0400

    Agregar archivos de proyecto.

---
+++ ProyectoDeEjemplos/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ProgramaPrincipal
+{
+    
+    internal class Programa
+    {
+        static string nombre = "Pepe";
+        static void Main(string[] args)
+        {
+            int edad = 20;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(nombre);
+            Console.WriteLine(edad);
+            OtroMain(null);
+        }
+
+        static void OtroMain(string[] args) {
+            Console.WriteLine("Hola mundo, dice " + nombre);
+            //Console.WriteLine("La edad es " + edad);
+        }
+    }
+
+    public class EjemploClase { 
+    
+    }
+}
+
+namespace Auxiliares {
+    namespace Desarrollo {
+        public class OtraClase {
+            
+        }
+    }
+}
