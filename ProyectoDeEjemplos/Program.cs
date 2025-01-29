a0d3a0c3ff73aabf49642978d3b512b6e52b244c
WinTux <rusok.software@gmail.com>
2025-01-28 21:20:20 -0400

    Tipos de datos y args

--- ProyectoDeEjemplos/Program.cs
+++ ProyectoDeEjemplos/Program.cs
@@ -11,6 +11,63 @@
             Console.WriteLine(nombre);
             Console.WriteLine(edad);
             OtroMain(null);
+
+            //Numéricos
+            int num = 123; // 123, 0, -2, 2000000
+            short num2 = 123; // 123
+            byte num3 = 123; // 123
+            long num4 = 123; // 123, 2000000
+
+            float num5 = 12.3f; // 12.3
+            double num6 = 12.3; // 0.123e2
+            num6 = 123.56; // 0.12356e3
+            num6 = 0.00034; // 0.34e-3
+            num6 = 34.7f;
+            num6 = 45; // Z < R
+            num5 = num;
+
+            //Texto
+            char letra;
+            letra = 'w';
+            //letra = 'que';
+            letra = '8';
+            letra = '?';
+
+            string txt;
+            txt = "Pepe";
+            txt = "Juan \"Pepe\" Perales";
+            txt = "Pepe\nPerales";
+            txt = "Pepe\tPerales";
+            txt = "";
+            Console.WriteLine(txt);
+
+            //Logico
+            bool casado; // Álgebra booleana, lógica propicisional
+            casado = false;
+            casado = true;
+
+            // Arreglos
+            // Unidimensionales
+            string[] roperoMama;
+            roperoMama= new string[4];
+            roperoMama[2] = "Blusa";
+            roperoMama[3] = "Zapatos";
+
+            /*
+             
+             [     ] 0 
+             [     ] 1 
+             [Blusa] 2 
+             [ zap ] 3 
+             
+             [][][Blusa][Zap]
+              0 1 2 3
+
+             */
+            Console.WriteLine(roperoMama[2]);
+            Console.WriteLine(args[2]);
+            Console.WriteLine(args[1]);
+            Console.WriteLine(args[0]);
         }
 
         static void OtroMain(string[] args) {
