115ab03d126819e5cc4c3f6670bab4e1c53b5adf
WinTux <rusok.software@gmail.com>
2025-01-29 21:22:47 -0400

    Estructuras iterativas

--- ProyectoDeEjemplos/Program.cs
+++ ProyectoDeEjemplos/Program.cs
@@ -59,15 +59,104 @@
              [     ] 1 
              [Blusa] 2 
              [ zap ] 3 
+            ultimoIndice = cantidad - 1
              
              [][][Blusa][Zap]
               0 1 2 3
 
              */
             Console.WriteLine(roperoMama[2]);
-            Console.WriteLine(args[2]);
-            Console.WriteLine(args[1]);
-            Console.WriteLine(args[0]);
+            if (args.Length > 0 && args.Length % 2 == 0)
+            {
+                /*
+                if (args[0] == "-a" && args.Length == 2)
+                {
+                    Console.WriteLine("Procesando documento " + args[1] + " con el algoritmo Alfa");
+                }
+                else {
+                    if (args[0] == "-b" && args.Length == 2)
+                    {
+                        Console.WriteLine("Procesando documento " + args[1] + " con el algoritmo Beta");
+                    }
+                    else {
+                        if (args[0] == "-c" && args.Length == 2)
+                        {
+                            Console.WriteLine("Procesando documento " + args[1] + " con el algoritmo Gamma");
+                        }
+                        else {
+                            Console.WriteLine("Se esperaba con argumento -a, -b o -c");
+                        }
+                    }
+                }*/
+                int indice = 0;
+                while (indice < args.Length)
+                {
+                    if (args[indice] == "-a" && args[indice + 1] != null)
+                    {
+                        Console.WriteLine("Procesando documento " + args[indice + 1] + " con el algoritmo Alfa");
+                    }
+                    else {
+                        if (args[indice] == "-b" && args[indice + 1] != null)
+                        {
+                            Console.WriteLine("Procesando documento " + args[indice + 1] + " con el algoritmo Beta");
+                        }
+                        else {
+                            if (args[indice] == "-c" && args[indice + 1] != null)
+                            {
+                                Console.WriteLine("Procesando documento " + args[indice + 1] + " con el algoritmo Gamma");
+                            }
+                        }
+                    }
+
+                    indice++;//indice = indice + 1;
+                }
+            }
+            else {
+                Console.WriteLine("Se necesitan argumentos como: -a, -b, -c seguido de un archivo pdf");
+            }
+
+            // Estructuras iterativas
+            // While (Mientras)
+            // Do-While (Hacer-Mientras)
+            num = 10;
+            do {
+                Console.WriteLine("Estoy en Do-While con indice: " + num);
+                num++;
+            } while (num < 5);
+            // for (Para)
+            for (int i=5;i > 0;i--) {
+                Console.WriteLine("falta "+ i);
+            }
+            Console.WriteLine("Feliz año nuevo!!");
+            for (int i = 1; i<=5;i++) {
+                Console.WriteLine("Pasó " + i);
+            }
+            for (int i = 0; i <= 10;i=i+3) {
+                Console.WriteLine("El valor de i: "+i);
+            }
+            for (int i =0, j = 100;i <100 && j > 0;i++,j-- ) {
+                Console.WriteLine("i: " + i+"; j: "+j);
+            }
+            // Arreglo bidimensional
+            int[,] abc = new int[4,4];
+            /*
+            [ ][ ][ ][ ]
+            [ ][ ][ ][ ]
+            [ ][ ][ ][ ]
+            [ ][ ][ ][ ]
+
+             */
+            int[,] xyz = new int[4,2];
+            /*
+            [ ][ ][ ][ ]
+            [ ][ ][ ][ ]
+
+
+            [ ][ ]
+            [ ][ ]
+            [ ][ ]
+            [ ][ ]
+             */
         }
 
         static void OtroMain(string[] args) {
